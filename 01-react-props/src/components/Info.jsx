1461f30f5883c2acd21d2306525caa938f2f1488
César Guerra <warderer@gmail.com>
2025-08-12 21:32:55 -0600

    ✨ Add components info

---
+++ 01-react-props/src/components/Info.jsx
@@ -0,0 +1,12 @@
+const Info = () => {
+  return (
+    <section className="info">
+        <div className="contenedor">
+            <h2 className="titulo left">Juntos podemos apoyar con props</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </section>
+  )
+}
+
+export default Info
