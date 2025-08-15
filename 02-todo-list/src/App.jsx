2d4a39238f79fcfd9fbd4fc768b5b11feb2e4344
César Guerra <warderer@gmail.com>
2025-08-14 19:43:32 -0600

    💄 ToDo List Base UI

--- 02-todo-list/src/App.jsx
+++ 02-todo-list/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
 
-function App() {
-
+function App () {
   return (
     <>
+      <h1>Todo List</h1>
+      <input type='text' placeholder='Escribe una tarea' />
+      <button>Añadir</button>
 
+      {/* ul>li{Item $}*3>button{Eliminar} */}
+      <ul>
+        <li>Item 1<button>Eliminar</button></li>
+        <li>Item 2<button>Eliminar</button></li>
+        <li>Item 3<button>Eliminar</button></li>
+      </ul>
     </>
   )
 }
