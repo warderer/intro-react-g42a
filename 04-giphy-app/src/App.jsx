e1f3a3708d2ff41fe257f3f6a4340bfb8fa329ae
César Guerra <warderer@gmail.com>
2025-08-21 19:48:00 -0600

    ✨ Add API Call to trends endpoint

--- 04-giphy-app/src/App.jsx
+++ 04-giphy-app/src/App.jsx
@@ -1,10 +1,22 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
+function App () {
+  const [gifs, setGifs] = useState([])
+  const API_KEY = import.meta.env.VITE_GIPHY_API_KEY
+
+  useEffect(() => {
+    const fetchGifs = async () => {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=20&offset=0&rating=g&bundle=messaging_non_clips`)
+      const results = await response.json()
+      setGifs(results.data)
+    }
+    fetchGifs()
+  }, [])
 
   return (
     <>
-
+      <h1>Giphy App</h1>
     </>
   )
 }
