8ef6e7b452dfcd423da896ada9c73de738f25933
César Guerra <warderer@gmail.com>
2025-08-21 19:14:12 -0600

    ✨ Add ImageCard component

---
+++ 04-giphy-app/src/components/ImageCard.jsx
@@ -0,0 +1,9 @@
+const ImageCard = ({ url, title }) => {
+  return (
+    <div className='imagen-container'>
+      <img src={url} alt={title} className='image-view' />
+    </div>
+  )
+}
+
+export default ImageCard
