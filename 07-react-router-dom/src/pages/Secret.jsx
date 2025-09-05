c778ebebd385c2e4e2c00ca439ae811f8d9e2da9
César Guerra <warderer@gmail.com>
2025-09-04 18:52:28 -0600

    ✨ Add secret page for autenticated users

---
+++ 07-react-router-dom/src/pages/Secret.jsx
@@ -0,0 +1,11 @@
+const Secret = () => {
+  return (
+    <div className='container'>
+      <h1>Sección Secreta</h1>
+      <p>Solo un usuario autenticado puede ver esta página</p>
+      <p>¡Ahora estas autenticado!</p>
+    </div>
+  )
+}
+
+export default Secret
